8c4527d3c69340cc599f22776d297cace2956da8
enderflop <agbarloon@uiowa.edu>
2023-06-02 19:10:30 -0500

    about to condense metadata functions

--- helper_scripts/post-archive.py
+++ helper_scripts/post-archive.py
@@ -1,8 +1,8 @@
-#this script puts the number of files in each folder into the folders name
-#it also renames at least one file in each directory to "preview"
-
 import os
+import json
 from PIL import Image, ImageOps
+
+# A bunch of utilities to run before each commit to generate metadata, thumbnails, and everything else.
 
 def rename_folders():
     print("Renaming Folders")
@@ -36,7 +36,7 @@ def generate_thumbnails():
         photos = os.listdir(path)
         for img_file in photos:
             image_name = img_file.split(".")[0]
-            #skip jsons...................thumbnails....................and images with thumbnails already
+            #skip jsons...................thumbnails.................and images with thumbnails already
             if (("json" in img_file) or ("thumbnail" in img_file) or (f"{image_name}_thumbnail.jpeg" in photos)):
                 continue
             new_path = os.path.join(path, img_file)
@@ -51,20 +51,47 @@ def delete_thumbnails():
         path = os.path.join(".\photos", dir_name)
         photos = os.listdir(path)
         for img_file in photos:
-            #skip jsons...................thumbnails....................and images with thumbnails already
             if "thumbnail" in img_file:
                 os.remove(f".\photos\{dir_name}\{img_file}")
 
+#page will fetch and read folder_meta.json, displaying each folder and the thumbnail. We will generate the link to the next html page in javascript
 def generate_folder_list():
-    pass
+    artist_json = {}
+    for folder in os.listdir(".\photos"):
+        folder_dict = {}
+        folder_dict["name"] = folder
+        folder_url_name = folder.replace(" ", "%20") #spaces don't autoserialize themselves, you know.
+        folder_dict["thumbnail_url"] = f"https://raw.githubusercontent.com/EnderFlop/iowacitygraffiti/main/photos/{folder_url_name}/PREVIEW_thumbnail.jpeg"
+        artist_json[folder] = folder_dict
+    print(artist_json)
+    obj = json.dumps(artist_json)
+    with open(".\\folder_meta.json", "w") as outfile:
+        outfile.write(obj)
 
-def generate_photo_list(folder):
-    pass
-
+#page will fetch and read artist_meta.json, linking to the full image and displaying each thumbnail.
+def generate_photo_list():
+    meta_json = []
+    for folder in os.listdir(".\photos"):
+        for file in os.listdir(f".\photos\{folder}"):
+            photo_dict = {}
+            if ".jpg" not in file:
+                continue
+            name = file[:-4] #remove ".jpg"
+            photo_dict["folder"] = folder
+            photo_dict["name"] = name
+            folder_url_name = folder.replace(" ", "%20")
+            photo_dict["thumbnail_url"] = f"https://raw.githubusercontent.com/EnderFlop/iowacitygraffiti/main/photos/{folder_url_name}/{name}_thumbnail.jpeg"
+            photo_dict["full_url"] = f"https://raw.githubusercontent.com/EnderFlop/iowacitygraffiti/main/photos/{folder_url_name}/{name}.jpg"
+            meta_json.append(photo_dict)
+    obj = json.dumps(meta_json)
+    with open(".\\artist_meta.json", "w") as outfile:
+        outfile.write(obj)
 
 if __name__ == "__main__":
-    rename_preview()
+    #rename_preview()
     #files are renamed to preview BEFORE thumbnails are generated, keep this order.
-    generate_thumbnails()
-    rename_folders()
+    #generate_thumbnails()
+    #rename_folders()
+    #generate_folder_list()
+    generate_photo_list()
     pass
