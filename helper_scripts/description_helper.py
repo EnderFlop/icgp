dc799dd600c2565bdfc37c275af29643f1d78414
enderflop <agbarloon@uiowa.edu>
2023-06-10 19:05:26 -0500

    archived but not organized elrays roof and south dodge st bridge archive

--- helper_scripts/description_helper.py
+++ helper_scripts/description_helper.py
@@ -9,11 +9,22 @@ def get_image_name(img_path):
     img_path = img_path.split(".")[0]
     return img_path
 
+def get_location_name(img_path):
+    img_path = img_path.split("\\")[-2]
+    return img_path
 
-def create_description(date, location):
+def get_date(img_path):
+    img_path = img_path.split("\\")[-3]
+    img_path = img_path.replace(".", "/")
+    return img_path
+
+#files are organized ...../TEMP/{Location Name}/{Image Name}.jpg
+def create_description():
     img_path = sys.argv[1]
     img_name = get_image_name(img_path)
-    print(f"LOCATION: {location}")
+    location = get_location_name(img_path)
+    date = get_date(img_path)
+    print(f"\nNAME: {img_name}\nLOCATION: {location}\nDATE: {date}\n")
     artist = input("Artist: ")
     notes = input("Notes: ")
     data = {
@@ -42,6 +53,4 @@ def create_description(date, location):
     shutil.move(img_path, artist_folder)
 
 #update below values depending on when and were the photos were taken
-DATE = "6/1/2023"
-LOCATION = "Elray's Alley"
-create_description(DATE, LOCATION)
+create_description()
