5f79b60473f9fd2a0f94a910d55f06b1a1d0e113
enderflop <agbarloon@uiowa.edu>
2023-06-14 23:46:29 -0500

    beginning work on maps, struggling to load google api

--- piece.js
+++ piece.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const urlParams = new URLSearchParams(queryString);
   
   loadPiece()
+  loadMap()
 
   function loadPiece(){
     const artist = urlParams.get("artist");
@@ -20,13 +21,31 @@ window.addEventListener('DOMContentLoaded', () => {
     fetch(`https://raw.githubusercontent.com/EnderFlop/iowacitygraffiti/main/photos/${artist}/${imgName}.json`)
           .then(response => response.json())
           .then(data => {
-            console.log(data)
             photoTitleBarText.innerHTML = data["img_name"]
             Object.entries(data).forEach(item => {
               const listElem = document.createElement("li")
               listElem.innerHTML = item[0] + ": " + (item[1] ? item[1] : "N/A")
               metaList.appendChild(listElem)
             })
+    })
+  }
+
+  async function loadMap() {
+    const artist = urlParams.get("artist");
+    const imgName = urlParams.get("imgName");
+
+    fetch('https://raw.githubusercontent.com/EnderFlop/iowacitygraffiti/main/artist_meta.json')
+        .then(response => response.json())
+        .then(async data => {
+          photos = data[artist]["photos"]
+          myPhoto = photos.find(item => item["name"] === imgName)
+          myLat = myPhoto["lat"]
+          myLon = myPhoto["lon"]
+          const { Map } = await google.maps.importLibary("maps");
+          map = new Map(document.querySelector("#map-window-body"), {
+            center: {lat: myLat, lng: myLon},
+            zoom: 8
+          })
     })
   }
 })
